5a0a97650fb53b234391f15d7f688b05a996454c
Da5u <75331594+Da5u@users.noreply.github.com>
2020-12-02 04:45:42 +0300

    Update MPITest.sh
    Removed MPITestResults.sh from this script

--- MPITest.sh
+++ MPITest.sh
@@ -38,29 +38,3 @@ mpirun -np 2 IMB-MPI1 PingPong -off_cache -1  2>&1 | tee -a MPICHBenchmarkTestRe
 
 TimeStamp=$(date +%d.%m.%Y_%T)
 echo "MPITest stage: Ending of MPITest.sh at $TimeStamp"
-*******************************************************************************
-
-*******************************************************************************
-MPITestResults.sh**************************************************************
-*******************************************************************************
-#!/bin/bash
-
-# Defining of general build parameters
-TimeStamp=$(date +%d.%m.%Y_%T)
-if [ -z "$MPITestResultsDir" ]; then
-	MPITestResultsDir=MPITestResults
-fi
-echo "MPITestResults stage: Starting of MPITestResults.sh script with the following parameters: from directory $MPITestResultsDir at $TimeStamp"
-
-echo "MPITestResults stage: Performing of log clean-up"
-rm MPITestResultsLog.txt
-
-echo "MPITestResults stage: Performing of test results clean-up"
-rm -r $MPITestResultsDir
-mkdir $MPITestResultsDir
-cd $MPITestResultsDir || return $?
-
-#HERE is need to perform parsing of data (language and framework for it & Python) and fra and getting figures, crating of any simple test, such as MPICH results in better in each point than Intel MPI...
-
-TimeStamp=$(date +%d.%m.%Y_%T)
-echo "MPITestResults stage: Ending of MPITestResults.sh at $TimeStamp"
